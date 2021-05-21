342471b07667c722f9749a46758bab68da5bf2b3
Gyu <melon9751@naver.com>
2021-05-21 14:52:29 +0900

    유효성 처리 개선
    유효성 처리에서 null이 들어올 경우 개선

--- src/main/java/com/example/controller/ControllerAdvice.java
+++ src/main/java/com/example/controller/ControllerAdvice.java
@@ -1,11 +1,8 @@
 package com.example.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.example.dto.Error;
 import com.example.dto.ErrorResponse;
-
+import errors.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -13,7 +10,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import errors.UserNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public class ControllerAdvice {
@@ -31,7 +29,9 @@ public class ControllerAdvice {
             FieldError fieldError = (FieldError) error;
             String field = fieldError.getField();
             String message = fieldError.getDefaultMessage();
-            String invalidValue = fieldError.getRejectedValue().toString();
+            String invalidValue = null;
+            if(fieldError.getRejectedValue() != null)
+                invalidValue = fieldError.getRejectedValue().toString();
             Error errorMessage = Error.builder()
                     .field(field)
                     .message(message)
